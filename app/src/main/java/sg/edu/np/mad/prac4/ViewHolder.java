182bc2cccdfa5f1481726ebe28e01c0cd4c5e06d
Wee Kiat Ernest Toh <s10221816@connect.np.edu.sg>
2023-06-17 00:21:02 +0800

    prac6

---
+++ app/src/main/java/sg/edu/np/mad/prac4/ViewHolder.java
@@ -0,0 +1,21 @@
+package sg.edu.np.mad.prac4;
+
+import android.media.Image;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    ImageView image;
+    TextView txt;
+    TextView txtdes;
+    public ViewHolder(View itemView){
+        super(itemView);
+        image = itemView.findViewById(R.id.imageView3);
+        txt = itemView.findViewById(R.id.textView3);
+        txtdes = itemView.findViewById(R.id.textView4);
+    }
+
+}
